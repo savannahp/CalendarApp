2135b2084ab1e89aec9fdff9ec02889c0636f113
Paul Land <paul.land1993@gmail.com>
2018-02-28 17:41:44 -0700

    changed filename

--- app/src/main/java/teamproject/savannahpyle/calendarapp/HomePage.java
+++ app/src/main/java/teamproject/savannahpyle/calendarapp/HomePage.java
@@ -3,7 +3,7 @@ package teamproject.savannahpyle.calendarapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/test/java/teamproject/savannahpyle/calendarapp/CalendarAppTests.java
@@ -0,0 +1,21 @@
+package teamproject.savannahpyle.calendarapp;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by paulland on 2/28/18.
+ *
+ * @author Paul and Savannah
+ */
+
+public class CalendarAppTests {
+
+    public void UserIsNotNull() {
+
+    }
+
+}
