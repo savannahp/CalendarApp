8fb4693880b0cc2cf5574327937f2fd0f767d2b6
Paul Land <paul.land1993@gmail.com>
2018-03-07 19:06:15 -0700

    added stuff for shared prefs

--- app/src/main/java/teamproject/savannahpyle/calendarapp/HomeActivity.java
+++ app/src/main/java/teamproject/savannahpyle/calendarapp/HomeActivity.java
@@ -1,10 +1,17 @@
 package teamproject.savannahpyle.calendarapp;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    static final String KEY_ACTIVITY = "teamproject.savannahpyle.calendarapp";
 
     private User user;
 
@@ -13,6 +20,22 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = new ContextWrapper(getApplicationContext());
+
+        // Load the preferences
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String activity = preferences.getString(KEY_ACTIVITY, "No saved state");
+
+        if (activity.equals("MonthActivity")) {
+            // Create intent and put the scripture information in it.
+            Intent intent = new Intent(this, MonthActivity.class);
+            startActivity(intent);
+        }
+
+
+//        SharedPreferences preferences = getSharedPreferences(KEY_ACTIVITY, MODE_PRIVATE);
+//        String mostRecentActivity = preferences.getString("mostRecentActivity", null);
     }
 
     // TODO: Add stuff
--- app/src/main/java/teamproject/savannahpyle/calendarapp/MonthActivity.java
+++ app/src/main/java/teamproject/savannahpyle/calendarapp/MonthActivity.java
@@ -1,13 +1,36 @@
 package teamproject.savannahpyle.calendarapp;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MonthActivity extends AppCompatActivity {
 
+//    static final String KEY_ACTIVITY = "teamproject.savannahpyle.calendarapp";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_main);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Context context = new ContextWrapper(getApplicationContext());
+
+        // Create editor for shared preferences and add the scripture parts using intent
+        SharedPreferences.Editor edit = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        edit.putString(HomeActivity.KEY_ACTIVITY, "MonthActivity");
+        edit.apply();
+
+//        SharedPreferences.Editor editor = getSharedPreferences(KEY_ACTIVITY, MODE_PRIVATE).edit();
+//
+//        editor.putString("mostRecentActivity", "MonthActivity");
+//        editor.apply();
     }
 }
